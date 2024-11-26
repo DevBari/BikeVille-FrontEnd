7065784a78913905965c9282a29fcb30d01e51d9
luigi cascella <luigicascella5@gmail.com>
2024-11-26 17:31:54 +0100

    navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./components/home/home.component";
+import { FormsModule } from '@angular/forms';
+import { NgStyle } from '@angular/common';
 
 @Component({
   
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, FormsModule, NgStyle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
   
@@ -15,5 +17,5 @@ import { HomeComponent } from "./components/home/home.component";
 export class AppComponent {
 
   title = 'BikeVille';
-  
+
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { CommonModule, NgStyle } from '@angular/common';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
+
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgStyle, FormsModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
+
 })
+
 export class NavbarComponent {
+
+  isHidden = false; // True per nascondere l'elemento
+  isXlScreen = window.innerWidth >= 1280; // Condizione per schermi XL
+
+  // Aggiungi un listener per aggiornare isXlScreen durante il ridimensionamento della finestra
+  
+  constructor() {
+
+    window.addEventListener('resize', () => {
+      this.isXlScreen = window.innerWidth >= 1280;
+    });
+    
+  }
 
 }
